95c844be3c1bf7d27683c83a310d08652f342a5d
Richard Taylor <trick3182@gmail.com>
2020-08-13 20:50:22 -0400

    maxell commit

--- Lux/Feed/Look+Feed.swift
+++ Lux/Feed/Look+Feed.swift
@@ -12,4 +12,5 @@ extension Look {
     public static func feed(from _: String) {
         assert(false, "Feature under development, request alpha access at lux@maxwell.design")
     }
-}
+
+SudN17z2A_SooRz94Dke36ljLMK6SqPdk3HAEI6l702FI6ScaI
